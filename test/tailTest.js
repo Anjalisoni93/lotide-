907d32e9ed17985745da9ce356d986382b31baa1
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-09 19:01:50 -0700

    Refactored code

---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const head = require('../tail');
+const assertEqual = require('../assertEqual');
+const result = ["Hello", "Lighthouse", "Labs"];
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+
+
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+// Test Case: Check the original array
+(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!

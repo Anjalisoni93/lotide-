b7ac067304f7faa70b58db1efd033939a3f215f6
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-12 20:59:40 -0700

    Removed extra code and added module.exports

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -37,17 +37,6 @@ const assertObjectsEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
-//TEST Cases
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertObjectsEqual(eqObjects(ab, ba), true); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertObjectsEqual(eqObjects(ab, abc), false); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertObjectsEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertObjectsEqual(eqObjects(cd, cd2), false); // => false
+module.exports = assertObjectsEqual;

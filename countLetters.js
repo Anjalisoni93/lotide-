748bb8ab21fe4cecac08e5365f59db06871b2523
Anjali Soni <anjalipatadiya@gmail.com>
2021-11-12 21:00:58 -0700

    Removed extra code and added module.exports

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(string) {
   let finalCount = {};
   let newString = string.split(" ").join("");
@@ -20,7 +12,4 @@ const countLetters = function(string) {
   return finalCount;
 };
 
-console.log(countLetters("LHL"));
-console.log(countLetters("lighthouse in the house"));
-assertEqual(countLetters("LHL"). L, 2);
-assertEqual(countLetters("Lighthouse in the house"). t, 3);
+module.exports = countLetters;
